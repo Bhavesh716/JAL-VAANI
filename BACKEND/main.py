561233c8f0b6678227f844faf00cbfdab8ec06e5
Bhavesh Gudlani <bhaveshgudlani716@gmail.com>
2026-01-30 06:26:06 +0530

    Update main.py

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -5,6 +5,10 @@ from sqlalchemy.orm import sessionmaker, Session
 from BACKEND.latest_water_api import get_latest_water_snapshot
 from BACKEND.search_data_engine import get_district_stats
 from BACKEND.analysis_engine import get_analysis
+from BACKEND.prediction_engine import get_prediction
+
+import joblib
+recharge_model = joblib.load("../MODELS/recharge_rf_model.pkl")
 
 
 DATABASE_URL = "sqlite:///./users.db"
@@ -134,3 +138,25 @@ def district_stats(state: str, district: str):
 @app.get("/analysis_data")
 def analysis(state: str, district: str):
     return get_analysis(state, district)
+
+@app.get("/predict")
+def predict(state: str, district: str, date: str):
+
+    result = get_prediction(state, district, date)
+
+    if not result:
+        return {
+            "status": "no_data"
+        }
+
+    recharge = recharge_model.predict([[result["predicted_wl"]]])[0]
+
+    return {
+        "status": "ok",
+        "predicted_wl": result["predicted_wl"],
+        "predicted_recharge": round(float(recharge), 2),
+        "weekly_chart": {
+            "history": result["history"],
+            "prediction": result["prediction"]
+        }
+    }
